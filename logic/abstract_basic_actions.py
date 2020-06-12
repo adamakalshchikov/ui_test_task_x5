3aa91e8269be724fda924048ce1f2b906084a09a
a.damakalshchikov <a.damakalshchikov@atol.ru>
2020-06-12 13:38:42 +0300

    Добавил абстрактные методы

--- logic/abstract_basic_actions.py
+++ logic/abstract_basic_actions.py
@@ -11,3 +11,35 @@ class AbstractBasicActions(ABC):
     @abstractmethod
     def wait_for_element_present(self, by: By, error_message: str, timeout: int) -> WebElement:
        raise NotImplementedError
+
+    @abstractmethod
+    def wait_for_element_present_and_click(self, by: By, error_message: str) -> WebElement:
+        raise NotImplementedError
+
+    @abstractmethod
+    def wait_for_element_and_send_keys(self, by: By, value: str, error_message: str) -> WebElement:
+        raise NotImplementedError
+
+    @abstractmethod
+    def wait_for_element_not_present(self, by: By, error_message: str, timeout: int) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def wait_for_element_and_clear(self, by: By, error_message: str, timeout: int) -> WebElement:
+        raise NotImplementedError
+
+    @abstractmethod
+    def wait_for_all_elements(self, by: By, error_message: str, timeout: int) -> list[WebElement]:
+        raise NotImplemented
+
+    @abstractmethod
+    def wait_for_all_elements_are_not_presented(self, by: By, error_message: str, timeout: int) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_amount_of_elements(self, by: By) -> int:
+       raise NotImplementedError 
+
+    @abstractmethod
+    def wait_for_element_and_get_attribute(self, by:by, attribue: str, error_message: str, timeout: int) -> str:
+        raise NotImplementedError
